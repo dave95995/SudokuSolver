f13dd0ebf88ae5fa6ef44dd9966e34b5316c7d24
dave95995 <lex_dave@outlook.com>
2025-11-07 08:55:59 +0100

    Added example run.

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SudokuEngine engine = new("530070000600195000098000060800060003400803001700020006060000280000419005000080079");
+            Console.WriteLine("Sudoku puzzle");
+            engine.DisplayBoard();
+            Console.WriteLine();
+            if (engine.SolveWithDepthFirstSearch())
+            {
+                Console.WriteLine("Solution");
+                engine.DisplayBoard();
+            }
+            else
+            {
+                Console.WriteLine("Invalid puzzle.");
+            }
+
         }
     }
 }
